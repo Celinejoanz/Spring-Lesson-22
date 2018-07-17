376cee5cf9c3dd6193b20593062d8e5c2faf7053
Chidimma Uba <chidimmajuba@gmail.com>
2018-07-17 16:34:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Spring-Lesson-22" 
---
+++ src/main/java/com/example/springlesson_22/Springlesson22Application.java
@@ -0,0 +1,12 @@
+package com.example.springlesson_22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlesson22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springlesson22Application.class, args);
+	}
+}
